dfce441b7400e90c458db7bb2e780e87ba672c7b
farahmahmoud70 <farah.mahmoud7080@gmail.com>
2023-02-12 22:05:08 +0200

    handle tokens

--- src/handlers/user_routes.ts
+++ src/handlers/user_routes.ts
@@ -1,67 +1,72 @@
-import express, { Request, Response } from 'express'
-import { User, UserStore } from '../models/users'
+import express, { Request, Response } from "express";
+import { User, UserStore } from "../models/users";
+import verifyAuthToken from "../midllewares/global";
+import jwt from "jsonwebtoken";
 
-const store = new UserStore()
+const store = new UserStore();
 
 const index = async (_req: Request, res: Response) => {
-  const users = await store.index()
-  res.json(users)
-}
+  const users = await store.index();
+  res.json(users);
+};
 
 const show = async (req: Request, res: Response) => {
-   const user = await store.show(req.params.id)
-   res.json(user)
-}
+  const user = await store.show(req.params.id);
+  res.json(user);
+};
 
 const create = async (req: Request, res: Response) => {
-    try {
-        const user: User = {
-            first_name: req.body.first_name,
-            last_name: req.body.last_name,
-            balance: req.body.balance,
-            email: req.body.email,
-            password:req.body.password
-        }
+  const user: User = {
+    first_name: req.body.first_name,
+    last_name: req.body.last_name,
+    balance: req.body.balance,
+    email: req.body.email,
+    password: req.body.password,
+  };
 
-        const newUser = await store.create(user)
-        res.json(newUser)
-    } catch(err) {
-        res.status(400)
-        res.json(err)
-    }
-}
+  try {
+    const newUser = await store.create(user);
+    const token = jwt.sign(
+      { user: newUser },
+      process.env.NEW_USER_TOKEN as string
+    );
+    res.json(token);
+  } catch (err) {
+    res.status(400);
+    res.json(err);
+  }
+};
 
 const destroy = async (req: Request, res: Response) => {
-    const deleted = await store.delete(req.body.id)
-    res.json(deleted)
-}
+  const deleted = await store.delete(req.body.id);
+  res.json(deleted);
+};
 
 const update = async (req: Request, res: Response) => {
-    try {
-        //p.first_name, p.last_name, p.balance,p.email,p.password
-        const user: User = {
-            id: (req.params.id as unknown) as number,
-            first_name: req.body.first_name,
-            last_name: req.body.last_name,
-            balance: req.body.balance,
-            email: req.body.email,
-            password:req.body.password
-        }
-        const updateUser = await store.update(user)
-        res.json(updateUser)
-    } catch(err) {
-        res.status(400)
-        res.json(err)
-    }
-}
-
+  try {
+    //p.first_name, p.last_name, p.balance,p.email,p.password
+    const user: User = {
+      id: req.params.id as unknown as number,
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      balance: req.body.balance,
+      email: req.body.email,
+      password: req.body.password,
+    };
+    const updateUser = await store.update(user);
+    res.json(updateUser);
+  } catch (err) {
+    res.status(400);
+    res.json(err);
+  }
+};
 
 const userRoutes = (app: express.Application) => {
-  app.get('/users', index)
-  app.get('/users/:id', show)
-  app.post('/users', create)
-  app.delete('/users/:id', destroy)
-  app.patch('/users/:id', update)
-}
+  app.get("/users", index);
+  app.get("/users/:id", verifyAuthToken, show);
+  app.post("/users", create);
+  app.delete("/users/:id", verifyAuthToken, destroy);
+  app.patch("/users/:id", verifyAuthToken, update);
+};
 
-export default userRoutes 
+export default userRoutes;
--- src/handlers/watchList_routes.ts
+++ src/handlers/watchList_routes.ts
@@ -1,59 +1,60 @@
-import express, { Request, Response } from 'express'
-import { List, userList } from '../models/watchLists'
+import express, { Request, Response } from "express";
+import { List, userList } from "../models/watchLists";
+import verifyAuthToken from "../midllewares/global";
 
-const lst = new userList()
+const lst = new userList();
 
 const index = async (_req: Request, res: Response) => {
-  const movies = await lst.index()
-  res.json(movies)
-}
+  const movies = await lst.index();
+  res.json(movies);
+};
 
 const show = async (req: Request, res: Response) => {
-   const movie = await lst.show(req.params.id)
-   res.json(movie)
-}
+  const movie = await lst.show(req.params.id);
+  res.json(movie);
+};
 
 const create = async (req: Request, res: Response) => {
   try {
-      const list: List = {
-        user_id: req.body.user_id,
-        movie_id: req.body.movie_id
-      }
-      const newEntity = await lst.create(list)
-      res.json(newEntity)
-      console.log(newEntity)
-  } catch(err) {
-      console.log(err)
-      res.status(400)
-      res.json(err)
+    const list: List = {
+      user_id: req.body.user_id,
+      movie_id: req.body.movie_id,
+    };
+    const newEntity = await lst.create(list);
+    res.json(newEntity);
+    console.log(newEntity);
+  } catch (err) {
+    console.log(err);
+    res.status(400);
+    res.json(err);
   }
-}
+};
 
 const destroy = async (req: Request, res: Response) => {
-  const deleted = await lst.delete(req.body.id)
-  res.json(deleted)
-}
+  const deleted = await lst.delete(req.body.id);
+  res.json(deleted);
+};
 
 const update = async (req: Request, res: Response) => {
   try {
-      const movie: List = {
-          id: (req.params.id as unknown) as number,
-          user_id: req.body.user_id,
-          movie_id: req.body.movie_id
-      }
-      const updateMovie = await lst.update(movie)
-      res.json(updateMovie)
-  } catch(err) {
-      res.status(400)
-      res.json(err)
+    const movie: List = {
+      id: req.params.id as unknown as number,
+      user_id: req.body.user_id,
+      movie_id: req.body.movie_id,
+    };
+    const updateMovie = await lst.update(movie);
+    res.json(updateMovie);
+  } catch (err) {
+    res.status(400);
+    res.json(err);
   }
-}
+};
 const listRoutes = (app: express.Application) => {
-  app.get('/watchlist', index)
-  app.get('/watchlist/:id', show)
-  app.post('/watchlist', create)
-  app.delete('/watchlist/:id', destroy)
-  app.patch('/watchlist/:id', update)
-}
+  app.get("/watchlist", index);
+  app.get("/watchlist/:id", verifyAuthToken, show);
+  app.post("/watchlist", create);
+  app.delete("/watchlist/:id", verifyAuthToken, destroy);
+  app.patch("/watchlist/:id", verifyAuthToken, update);
+};
 
-export default listRoutes 
+export default listRoutes;
